c19ba3cdfb61257fe21717d2dc7167360d6e8a83
tgzz14 <108690992+tgzz14@users.noreply.github.com>
2022-07-04 18:20:58 -0500

    homework 02

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -99,48 +99,79 @@ function menosQueNoventa(num) {
   // Devuelve "true" si el argumento de la función "num" es menor que noventa
   // De lo contrario, devuelve "false"
   // Tu código:
+  if (num<90) {
+    return true; {
+      return false;
+    }
+  }
   
 }
+menosQueNoventa(120);
 
 function mayorQueCincuenta(num) {
   // Devuelve "true" si el argumento de la función "num" es mayor que cincuenta
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+  if (num > 50) {
+    return true; {
+      return false;
+    }
+  }
 }
+mayorQueCincuenta(40)
 
 function obtenerResto(x, y) {
   // Obten el resto de la división de "x" entre "y"
   // Tu código:
-  
+  var resto = x % y;
+  return resto;
 }
+obtenerResto(21,5);
 
 function esPar(num) {
   // Devuelve "true" si "num" es par
   // De lo contrario, devuelve "false"
   // Tu código:
+if (num % 2 == 0 ) {
+  return true; {
+    return false;
+  }
+}
   
 }
+esPar(20);
 
 function esImpar(num) {
   // Devuelve "true" si "num" es impar
   // De lo contrario, devuelve "false"
   // Tu código:
+  if (num % 2 !== 0) {
+    return true; {
+      return false;
+    }
+  }
   
 }
+
+esImpar(40);
 
 function elevarAlCuadrado(num) {
   // Devuelve el valor de "num" elevado al cuadrado
   // ojo: No es raiz cuadrada!
   // Tu código:
+  return Math.pow(num,2);
   
 }
+
+elevadorAlcuadrado(4);
 
 function elevarAlCubo(num) {
   // Devuelve el valor de "num" elevado al cubo
   // Tu código:
-  
+  return Math.pow(num,3);
 }
+
+elevadorAlCubo(3);
 
 function elevar(num, exponent) {
   // Devuelve el valor de "num" elevado al exponente dado en "exponent"
